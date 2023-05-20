02b46981ea2a102de4eea26b1ba07323f00c89a3
Thomas Liang <thliang01@gmail.com>
2023-05-20 10:30:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 
-st.header('st.selectbox')
+st.title('Customizing the theme of Streamlit apps')
 
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
+st.write('Contents of the `.streamlit/config.toml` file of this app')
 
-st.write('Your favorite color is ', option)
+st.code("""
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
